50332819fcea68b8000ce69b7a82cfda361b07e3
Lukas Moravec <sydnexpklover@gmail.com>
2019-10-29 18:30:52 +0100

    Initial commit

--- src/main/java/Gravityhook/Main.java
+++ src/main/java/Gravityhook/Main.java
@@ -1,11 +1,19 @@
 package Gravityhook;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
+
+
+    public Parent root = null;
 
     public static void main(String[] args) {
         launch(args);
@@ -13,8 +21,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Label l = new Label("Hello, world!");
-        Scene s = new Scene(l);
+        try {
+            this.root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException("Could not load FXML");
+        }
+
+        Scene s = new Scene(root);
         primaryStage.setScene(s);
         primaryStage.show();
 
